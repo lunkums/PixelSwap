28611f71a901d3f2fb63cba30c2dbb5574978139
lunkums <lukeleedy123@gmail.com>
2022-10-13 10:29:59 -0400

    Add script

---
+++ script.js
@@ -0,0 +1,33 @@
+function readSingleFile(evt) {
+  //Retrieve the first (and only!) File from the FileList object
+  let f = evt.target.files[0];
+
+  if (f) {
+    let r = new FileReader();
+    r.onload = function (e) {
+      let contents = e.target.result;
+      alert(
+        "Got the file.\n" +
+          "name: " +
+          f.name +
+          "\n" +
+          "type: " +
+          f.type +
+          "\n" +
+          "size: " +
+          f.size +
+          " bytes\n" +
+          "starts with: " +
+          contents.substr(0, contents.indexOf("\n"))
+      );
+      document.getElementById("area").value = contents;
+    };
+    r.readAsText(f);
+  } else {
+    alert("Failed to load file");
+  }
+}
+
+document
+  .getElementById("palette-input")
+  .addEventListener("change", readSingleFile, false);
